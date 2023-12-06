333f30c531ce1b0e07e955417a062f85dbaeb435
Dana AlSiddig <danlsiddig@gmail.com>
2023-12-06 20:13:52 +0300

    Feat: Single Actors Page

--- src/pages/actors/index.jsx
+++ src/pages/actors/index.jsx
@@ -17,7 +17,7 @@ export default function Actors({ actors }) {
     
      return (
       <div key={actor.id}>
-    <a href="#!">
+    <a href={`/actors/${actor.id}`}>
      <div
        className=" mx-3 mt-6 flex flex-col rounded-lg bg-white shadow-[0_2px_15px_-3px_rgba(0,0,0,0.07),0_10px_20px_-2px_rgba(0,0,0,0.04)] sm:shrink-0 sm:grow sm:basis-0"> 
          <img
